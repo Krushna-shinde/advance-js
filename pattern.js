67846858bbe2a0dcd668dc28c14ec08e5f527f20
Krushna-shinde <krishnashinde1996@gmail.com>
2023-04-09 19:02:35 +0530

    Pattern code

---
+++ pattern.js
@@ -0,0 +1,16 @@
+function pattern(n) {
+    let x = Math.floor(n / 2);
+    for (let i = 0; i <= n; i++) {
+        let bag = '';
+        for (let j = 0; j <= n; j++) {
+            if (i == 0 || i == n || j == 0 || j == n || i == x || j == x) {
+                bag = bag + "* ";
+            } else {
+                bag = bag + '  ';
+            }
+        }
+        //return bag;
+        console.log(bag);
+    }
+}
+pattern(10);
